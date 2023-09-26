231213bf6b596a69b98ea58f3e3d670421661957
lbavendanos <lbavendanos@gmail.com>
2023-09-26 16:33:17 -0500

    Fix home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,14 @@ export default function Home() {
 
   return (
     <section className="container my-10">
-      <div className="row justify-center md:row-cols-2 lg:row-cols-3">
-        <div className="col text-center">
-          <h1 className="text-5xl font-extrabold uppercase">{appName}</h1>
-          <p className="text-lg font-medium uppercase tracking-wider">
-            Abono orgánico
-          </p>
-          <p className="mt-4 text-center text-lg">
-            {`En ${appName} encuentra abono orgánico de calidad. Humus y compost que mejoran el suelo y promueven un crecimiento saludable de tus cultivos. ¡Potencia tu agricultura de forma sostenible!`}
-          </p>
-        </div>
+      <div className="text-center">
+        <h1 className="text-5xl font-extrabold uppercase">{appName}</h1>
+        <p className="text-lg font-medium uppercase tracking-wider">
+          Abono orgánico
+        </p>
+        <p className="mt-4 text-center text-lg">
+          {`En ${appName} encuentra abono orgánico de calidad. Humus y compost que mejoran el suelo y promueven un crecimiento saludable de tus cultivos. ¡Potencia tu agricultura de forma sostenible!`}
+        </p>
       </div>
     </section>
   )
