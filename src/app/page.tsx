65e615b8f40057c6fe9ccc1910c00b554b3ad0f9
lbavendanos <lbavendanos@gmail.com>
2023-10-15 23:27:24 -0500

    Add banner section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,38 @@
 import { config } from '@/lib/utils/helpers'
+import Image from 'next/image'
+import banner from './../../public/images/home/banner.png'
 
 export default function Home() {
   const appName = config('app.name')
 
   return (
-    <section className="container my-10">
-      <div className="text-center">
-        <h1 className="text-5xl font-extrabold uppercase">{appName}</h1>
-        <p className="text-lg font-medium uppercase tracking-wider">
-          Abono orgánico
-        </p>
-        <p className="mt-4 text-center text-lg">
-          {`En ${appName} encuentra abono orgánico de calidad. Humus y compost que mejoran el suelo y promueven un crecimiento saludable de tus cultivos. ¡Potencia tu agricultura de forma sostenible!`}
-        </p>
+    <section className="relative flex h-[500px] items-center lg:h-[600px]">
+      <div className="absolute left-0 top-0 -z-10 h-full w-full overflow-hidden">
+        <Image
+          alt="banner"
+          src={banner}
+          placeholder="blur"
+          quality={100}
+          fill
+          sizes="100vw"
+          className="object-cover"
+        />
+      </div>
+      <div className="container z-10 py-10">
+        <div className="grid grid-cols-12 gap-6">
+          <div className="col-span-12 lg:col-span-5">
+            <div className="text-shadow flex flex-col gap-y-4 text-primary-foreground shadow-primary/50">
+              <h1 className="text-center text-5xl font-extrabold leading-none lg:text-left lg:text-6xl">
+                Potencia tu agricultura de forma sostenible
+              </h1>
+              <p className="text-center text-lg leading-tight lg:text-left">
+                En {appName} encuentra abono orgánico de calidad. Humus y
+                Compost que mejoran el suelo y promueven un crecimiento
+                saludable de tus cultivos.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
