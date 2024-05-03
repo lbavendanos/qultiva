8f6acb177dcfcd5828543760b3d284037fe1e3e8
lbavendanos <lbavendanos@gmail.com>
2024-05-02 20:28:15 -0500

    Fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
-  ...eslintPluginAstro.configs['flat/recommended'],
+  ...eslintPluginAstro.configs.recommended,
   eslintPluginPrettierRecommended,
   {
     rules: {
