125454409fe3b355b39ea192bfb6daeeaf61139f
lbavendanos <lbavendanos@gmail.com>
2024-05-04 15:24:29 -0500

    Add navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,10 @@ import { fontFamily } from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js',
+  ],
   theme: {
     container: {
       center: true,
@@ -74,5 +77,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('flowbite/plugin'), require('tailwindcss-animate')],
 }
